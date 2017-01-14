8752b1e381a005b702f8926f2c2e5f4e074f843f
tobias <thinkdifferent055@gmail.com>
2017-01-14 22:48:19 +0100

    Diable trigger menu

--- PlayWall/src/de/tobias/playpad/viewcontroller/option/pad/PadSettingsViewController.java
+++ PlayWall/src/de/tobias/playpad/viewcontroller/option/pad/PadSettingsViewController.java
@@ -54,7 +54,7 @@ public class PadSettingsViewController extends NVC implements IPadSettingsViewCo
 		addTab(new GeneralPadTabViewController(pad));
 		addTab(new DesignPadTabViewController(pad));
 		addTab(new PlayerPadTabViewController(pad));
-		addTab(new TriggerPadTabViewController(pad));
+//		addTab(new TriggerPadTabViewController(pad)); TODO Add Trigger Tab when rewritten
 
 		if (pad.getContent() != null) {
 			try {
