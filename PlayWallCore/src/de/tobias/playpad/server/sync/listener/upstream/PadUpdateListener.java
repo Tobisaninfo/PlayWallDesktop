2c33cdcfbfc1d712e9f33a0da8b3eb8d9bb44aab
tobias <thinkdifferent055@gmail.com>
2017-02-21 18:20:14 +0100

    Start on Pad Update Listener (upstream)

---
+++ PlayWallCore/src/de/tobias/playpad/server/sync/listener/upstream/PadUpdateListener.java
@@ -0,0 +1,68 @@
+package de.tobias.playpad.server.sync.listener.upstream;
+
+import com.google.gson.JsonObject;
+import de.tobias.playpad.PlayPadPlugin;
+import de.tobias.playpad.pad.Pad;
+import de.tobias.playpad.project.Project;
+import de.tobias.playpad.project.page.Page;
+import de.tobias.playpad.server.Server;
+import de.tobias.playpad.server.sync.Commands;
+import de.tobias.playpad.server.sync.PropertyDef;
+import de.tobias.playpad.server.sync.ServerUtils;
+import javafx.beans.value.ChangeListener;
+
+import java.util.function.Consumer;
+
+/**
+ * Listen to the properties of Project to send changes to the server.
+ */
+public class PadUpdateListener {
+
+	private Pad pad;
+
+	private ChangeListener<String> nameListener;
+	private ChangeListener<Number> positionListener;
+
+	public PadUpdateListener(Pad pad) {
+		this.pad = pad;
+
+		nameListener = (observable, oldValue, newValue) -> {
+			handleInvalidation(json -> {
+				json.addProperty(PropertyDef.FIELD, PropertyDef.PAGE_NAME);
+				json.addProperty(PropertyDef.VALUE, newValue);
+			});
+		};
+
+		positionListener = (observable, oldValue, newValue) -> {
+			handleInvalidation(json -> {
+				json.addProperty(PropertyDef.FIELD, PropertyDef.PAGE_POSITION);
+				json.addProperty(PropertyDef.VALUE, newValue);
+			});
+		};
+	}
+
+	private void handleInvalidation(Consumer<JsonObject> handler) {
+		if (ServerUtils.isNewValueComingFromServer()) {
+			return;
+		}
+		JsonObject json = new JsonObject();
+		json.addProperty(PropertyDef.ID, pad.getUuid().toString());
+		json.addProperty(PropertyDef.PAD_PAGE, pad.getPage().getId().toString());
+		json.addProperty(PropertyDef.CMD, Commands.PAGE_UPDATE);
+
+		handler.accept(json);
+
+		Server server = PlayPadPlugin.getServerHandler().getServer();
+		server.push(json);
+	}
+
+	public void addListener() {
+		pad.nameProperty().addListener(nameListener);
+		pad.positionProperty().addListener(positionListener);
+	}
+
+	public void removeListener() {
+		pad.nameProperty().addListener(nameListener);
+		pad.positionProperty().removeListener(positionListener);
+	}
+}
