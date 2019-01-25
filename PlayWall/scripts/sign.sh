45e07bc6b4c73fdc755a1d6f73bf36a3b94da8fb
tobias <thinkdifferent055@gmail.com>
2019-01-25 17:17:56 +0100

    Add zip command to mac app script

--- PlayWall/scripts/sign.sh
+++ PlayWall/scripts/sign.sh
@@ -1,6 +1,8 @@
 if hash codesign 2>/dev/null; then
     codesign --force --verify --verbose --sign "Developer ID Application: Tobias Ullerich" PlayWall.app
     echo signed mac app
+
+    zip -r PlayWall.zip PlayWall.app
 else
     (>&2 echo "codesign image not found")
 fi
