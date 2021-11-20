0bb4becfc40bfb6b4d5a541535d936b3e6114989
tobias <thinkdifferent055@gmail.com>
2021-11-20 19:47:53 +0100

    #174 - Add PlayWallClient api

--- PlayWallPlugins/PlayWallPluginWebAPI/src/main/scala/de/tobias/playpad/plugin/api/WebApiPlugin.scala
+++ PlayWallPlugins/PlayWallPluginWebAPI/src/main/scala/de/tobias/playpad/plugin/api/WebApiPlugin.scala
@@ -6,15 +6,18 @@ import de.thecodelabs.plugins.versionizer.PluginArtifact
 import de.thecodelabs.storage.settings.{Storage, StorageTypes}
 import de.thecodelabs.utils.application.ApplicationUtils
 import de.thecodelabs.utils.application.container.PathType
+import de.thecodelabs.utils.threading.Worker
 import de.thecodelabs.utils.util.Localization
 import de.tobias.playpad.PlayPadPlugin
-import de.tobias.playpad.plugin.api.settings.{WebApiSettings, WebApiSettingsViewController}
+import de.tobias.playpad.api.{PlayPadClient, PlayPadClientImpl}
+import de.tobias.playpad.plugin.api.settings.{WebApiRemoteSettings, WebApiSettings, WebApiSettingsViewController}
 import de.tobias.playpad.plugin.api.websocket.WebSocketHandler
 import de.tobias.playpad.plugin.api.websocket.listener.{PadStatusListener, ProjectListener}
 import de.tobias.playpad.plugin.{Module, PlayPadPluginStub}
 import spark.{Request, Response, Spark}
 
 import java.nio.file.{Files, Path}
+import java.util
 
 class WebApiPlugin extends PlayPadPluginStub with PluginArtifact {
 
@@ -45,6 +48,19 @@ class WebApiPlugin extends PlayPadPluginStub with PluginArtifact {
 			Logger.info(f"Start WebAPI on port ${webApiSettings.getPort}")
 		}
 
+		webApiSettings.getRemoteSettings.forEach(remote => {
+			Worker.runLater(() => {
+				try {
+					val client = new PlayPadClientImpl(f"ws://${remote.getServerAddress}:${remote.getPort}/api")
+					WebApiPlugin.connections.put(remote, client)
+					client.connect(5)
+					Logger.info(s"Connected to remote PlayWall: ${remote.getName}")
+				} catch {
+					case e: Exception => Logger.error(e)
+				}
+			})
+		})
+
 		PlayPadPlugin.getInstance().addGlobalSettingsTab(() => new WebApiSettingsViewController(webApiSettings))
 		Localization.addResourceBundle("plugin/webapi/lang/base", getClass.getClassLoader)
 	}
@@ -60,4 +76,6 @@ class WebApiPlugin extends PlayPadPluginStub with PluginArtifact {
 
 object WebApiPlugin {
 	def getWebApiSettingsPath: Path = ApplicationUtils.getApplication.getPath(PathType.CONFIGURATION, "webapi.json")
+
+	var connections: util.Map[WebApiRemoteSettings, PlayPadClient] = new util.HashMap()
 }
